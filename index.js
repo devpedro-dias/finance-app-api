0ffe69a7f4032eb01866a3b6593e3b4e0182174f
Pedro Dias <pedrocorchogdev@gmail.com>
2025-04-14 20:29:57 -0300

    feat: add UpdateUserController

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import 'dotenv/config.js'
 import express from 'express'
 import { CreateUserController } from './src/controllers/create-user.js'
 import { GetUserByIdController } from './src/controllers/get-user-by-id.js'
+import { UpdateUserController } from './src/controllers/update-user.js'
 
 const app = express()
 
@@ -15,6 +16,14 @@ app.post('/api/users', async (request, response) => {
     response.status(statusCode).send(body)
 })
 
+app.patch('/api/users/:userId', async (request, response) => {
+    const updateUserController = new UpdateUserController()
+
+    const { statusCode, body } = await updateUserController.execute(request)
+
+    response.status(statusCode).send(body)
+})
+
 app.get('/api/users/:userId', async (request, response) => {
     const getUserByIdController = new GetUserByIdController()
 
@@ -24,5 +33,5 @@ app.get('/api/users/:userId', async (request, response) => {
 })
 
 app.listen(process.env.PORT, () =>
-    console.log(`Listening on port ${process.env.PORT}`)
+    console.log(`Listening on port ${process.env.PORT}`),
 )
