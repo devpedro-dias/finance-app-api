29b16bda353ac85c1449047bdec8aa829da6bed1
pedro dias <pedrocorchogdev@gmail.com>
2025-04-16 01:59:24 -0300

    refactor: add validate required fields helper function

--- src/controllers/helpers/validation.js
+++ src/controllers/helpers/validation.js
@@ -1,9 +1,33 @@
-import validator from 'validator'
-import { badRequest } from './http.js'
+import validator from "validator";
+import { badRequest } from "./http.js";
 
-export const checkIfIdIsValid = (id) => validator.isUUID(id)
+export const checkIfIdIsValid = (id) => validator.isUUID(id);
 
 export const invalidIdResponse = () =>
-    badRequest({
-        message: 'The provided id is not valid'
-    })
+  badRequest({
+    message: "The provided id is not valid",
+  });
+
+export const checkIfIsString = (value) => typeof value === "string";
+
+export const validateRequireFields = (params, requiredFields) => {
+  for (const field of requiredFields) {
+    const fieldIsMissing = !params[field];
+    const fieldIsEmpty =
+      checkIfIsString(params[field]) &&
+      validator.isEmpty(params[field], {
+        ignore_whitespace: true,
+      });
+
+    if (fieldIsMissing || fieldIsEmpty) {
+      return {
+        missingField: field,
+        ok: false
+      }
+    }
+  }
+  return {
+    ok: true,
+    missingField: undefined
+  }
+};
--- src/controllers/user/create-user.js
+++ src/controllers/user/create-user.js
@@ -7,6 +7,7 @@ import {
     badRequest,
     created,
     serverError,
+    validateRequireFields,
 } from '../helpers/index.js'
 
 export class CreateUserController {
@@ -24,10 +25,12 @@ export class CreateUserController {
                 'password',
             ]
 
-            for (const field of requiredFields) {
-                if (!params[field] || params[field].trim().length === 0) {
-                    return badRequest({ message: `Missing param: ${field}` })
-                }
+            const {ok: requiredFieldsWereProvided, missingField} = validateRequireFields(params, requiredFields)
+
+            if(!requiredFieldsWereProvided) {
+                return badRequest({
+                    message: `The field ${missingField} is required`
+                })
             }
 
             const passwordIsValid = checkIfPasswordIsValid(params.password)
