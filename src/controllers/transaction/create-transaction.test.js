787bcf75a0a84fbbb2883a7fe467a0b7bf7b9a16
Pedro Dias <pedrocorchogdev@gmail.com>
2025-04-21 18:43:13 -0300

    test: ensure CreateTransactionController returns 400 when missing amount

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -93,4 +93,18 @@ describe('CreateTransactionController', () => {
         // Assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 when missing amount', async () => {
+        // Arrange
+        const { sut } = makeSut()
+
+        // Act
+        const result = await sut.execute({
+            ...httpRequest,
+            body: { ...httpRequest.body, amount: undefined },
+        })
+
+        // Assert
+        expect(result.statusCode).toBe(400)
+    })
 })
