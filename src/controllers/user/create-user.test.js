31b4e9a29c7cd79aac948b2c900d80add56ef336
Pedro Dias <pedrocorchogdev@gmail.com>
2025-04-21 03:41:56 -0300

    test: ensure 400 is returned if email is not valid

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -70,7 +70,7 @@ describe('CreateUserController', () => {
     })
 
     it('should return 400 if email is not provided', async () => {
-        // arrange
+        // Arrange
         const createUserUseCase = new CreateUserUseCaseStub()
         const createUserController = new CreateUserController(createUserUseCase)
         const httpRequest = {
@@ -81,10 +81,30 @@ describe('CreateUserController', () => {
             },
         }
 
-        // act
+        // Act
         const result = await createUserController.execute(httpRequest)
 
-        // assert
+        // Assert
+        expect(result.statusCode).toBe(400)
+    })
+
+    it('should return 400 if email is not valid', async () => {
+        // Arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpRequest = {
+            body: {
+                first_name: 'Felipe',
+                last_name: 'Rocha',
+                email: 'fe',
+                password: '1234567',
+            },
+        }
+
+        // Act
+        const result = await createUserController.execute(httpRequest)
+
+        // Assert
         expect(result.statusCode).toBe(400)
     })
 })
