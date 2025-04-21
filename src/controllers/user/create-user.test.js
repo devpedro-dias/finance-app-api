c20d77f2ecb248e829f4a9fdf03ed5c11de72088
Pedro Dias <pedrocorchogdev@gmail.com>
2025-04-21 02:51:35 -0300

    test: ensure 400 is returned if last_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -48,4 +48,24 @@ describe('CreateUserController', () => {
         // Assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should returns 400 if last_name is not provided', async () => {
+        // Arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'John',
+                email: 'john.doe@example.com',
+                password: '123456',
+            },
+        }
+
+        // Act
+        const result = await createUserController.execute(httpRequest)
+
+        // Assert
+        expect(result.statusCode).toBe(400)
+    })
 })
