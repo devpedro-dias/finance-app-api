bf8532c33ce901a46a5cb5816e76627e8c84956a
Pedro Dias <pedrocorchogdev@gmail.com>
2025-04-21 16:53:31 -0300

    test: ensure UpdateUserController returns 200 if user is updated

---
+++ src/controllers/user/update-user.test.js
@@ -0,0 +1,45 @@
+import { faker } from '@faker-js/faker'
+import { UpdateUserController } from './update-user.js'
+
+describe('UpdateUserController', () => {
+    class UpdateUserUseCaseStub {
+        async execute(user) {
+            return user
+        }
+    }
+
+    const makeSut = () => {
+        const updateUserUseCaseStub = new UpdateUserUseCaseStub()
+        const sut = new UpdateUserController(updateUserUseCaseStub)
+
+        return { sut, updateUserUseCaseStub }
+    }
+
+    const httpRequest = {
+        params: {
+            userId: faker.string.uuid(),
+        },
+        body: {
+            first_name: faker.person.firstName(),
+            last_name: faker.person.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password({
+                length: 10,
+            }),
+        },
+    }
+
+    it('should return 200 if the user is updated', async () => {
+        // Arrange
+        const { sut } = makeSut()
+
+        // Act
+        const result = await sut.execute({
+            params: httpRequest.params,
+            body: httpRequest.body,
+        })
+
+        // Assert
+        expect(result.statusCode).toBe(200)
+    })
+})
