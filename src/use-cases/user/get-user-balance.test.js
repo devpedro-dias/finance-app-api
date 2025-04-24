3c706602ab9ccaaeb55996432c51264f54d3998b
pedro dias <pedrocorchogdev@gmail.com>
2025-04-24 01:28:38 -0300

    test: ensure GetUserBalanceUseCase throws if GetUserByIdRepository throws

--- src/use-cases/user/get-user-balance.test.js
+++ src/use-cases/user/get-user-balance.test.js
@@ -94,4 +94,20 @@ describe('GetUserBalanceUseCase', () => {
         // Assert 
         expect(executeSpy).toHaveBeenCalledWith(userId)
     })
+
+    it('should throw if GetUserByIdRepository throws', async () => {
+        // Arrange 
+        const { sut, getUserByIdRepository} = makeSut()
+
+        jest.spyOn(getUserByIdRepository, 'execute').mockRejectedValueOnce(
+            new Error()
+        )
+        const userId = faker.string.uuid()
+
+        // Act
+        const promise = sut.execute(userId)
+
+        // Assert
+        await expect(promise).rejects.toThrow()
+    })
 })
