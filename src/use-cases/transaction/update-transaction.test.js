7c4aad7aa9c48f911edd38e9bcbe40235f52f996
pedro dias <pedrocorchogdev@gmail.com>
2025-04-24 05:44:13 -0300

    test: ensure UpdateTransactionUseCase updates transaction successfully

---
+++ src/use-cases/transaction/update-transaction.test.js
@@ -0,0 +1,47 @@
+import { faker } from '@faker-js/faker'
+import { UpdateTransactionUseCase } from './update-transaction'
+
+describe('UpdateTransactionUseCase', () => {
+    const transaction = {
+        id: faker.string.uuid(),
+        user_id: faker.string.uuid(),
+        name: faker.commerce.productName(),
+        date: faker.date.anytime().toISOString(),
+        type: 'EXPENSE',
+        amount: Number(faker.finance.amount()),
+    }
+
+    class UpdateTransactionRepositoryStub {
+        async execute(transactionId) {
+            return {
+                id: transactionId,
+                ...transaction,
+            }
+        }
+    }
+
+    const makeSut = () => {
+        const updateTransactionRepository =
+            new UpdateTransactionRepositoryStub()
+            
+        const sut = new UpdateTransactionUseCase(updateTransactionRepository)
+
+        return {
+            sut,
+            updateTransactionRepository,
+        }
+    }
+
+    it('should create a transaction successfully', async () => {
+        // Arrange
+        const { sut } = makeSut()
+
+        // Act
+        const result = await sut.execute(transaction.id, {
+            amount: Number(faker.finance.amount()),
+        })
+
+        // Assert
+        expect(result).toEqual(transaction)
+    })
+})
