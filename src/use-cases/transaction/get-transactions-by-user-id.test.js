9a6eae91959d47b03b1adce7d0a54549d7a13044
pedro dias <pedrocorchogdev@gmail.com>
2025-04-24 05:39:14 -0300

    test: ensure GetTransactionsByUserIdUseCase calls GetUserByIdRepository with correct params

--- src/use-cases/transaction/get-transactions-by-user-id.test.js
+++ src/use-cases/transaction/get-transactions-by-user-id.test.js
@@ -65,4 +65,21 @@ describe('GetTransactionsByUserIdUseCase', () => {
         // Assert
         await expect(promise).rejects.toThrow(new UserNotFoundError(userId))
     })
+
+    it('should call GetUserByIdRepository with correct params', async () => {
+        // Arrange
+        const { sut, getUserByIdRepository } = makeSut()
+
+        const executeSpy = jest.spyOn(
+            getUserByIdRepository,
+            'execute',
+        )
+        const userId = faker.string.uuid()
+
+        // Act
+        await sut.execute(userId)
+
+        // Assert
+        expect(executeSpy).toHaveBeenCalledWith(userId)
+    })
 })
