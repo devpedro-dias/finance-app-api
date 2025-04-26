7c3a8b022995c3be8f70fedc0ac9e1e81e88acd3
Pedro Dias <pedrocorchogdev@gmail.com>
2025-04-26 20:34:30 -0300

    feat: ensure DeleteTransactionController returns 404 if TransactionNotFoundError is thrown

--- src/controllers/transaction/delete-transaction.js
+++ src/controllers/transaction/delete-transaction.js
@@ -1,3 +1,4 @@
+import { TransactionNotFoundError } from '../../errors/transaction.js'
 import {
     checkIfIdIsValid,
     invalidIdResponse,
@@ -24,12 +25,11 @@ export class DeleteTransactionController {
                     httpRequest.params.transactionId,
                 )
 
-            if (!deletedTransaction) {
-                return transactionNotFoundResponse()
-            }
-
             return ok(deletedTransaction)
         } catch (error) {
+            if (error instanceof TransactionNotFoundError) {
+                return transactionNotFoundResponse()
+            }
             console.error(error)
             return serverError(error)
         }
