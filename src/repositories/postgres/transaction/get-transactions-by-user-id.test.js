121440adb58ab1bf3e01795359bd71288ae24e46
Pedro Dias <pedrocorchogdev@gmail.com>
2025-04-26 06:39:07 -0300

    test: ensure PostgresGetTransactionsByUserIdRepository calls Prisma with correct params

--- src/repositories/postgres/transaction/get-transactions-by-user-id.test.js
+++ src/repositories/postgres/transaction/get-transactions-by-user-id.test.js
@@ -28,4 +28,17 @@ describe('PostgresGetTransactionsByUserIdRepository', () => {
             dayjs(transaction.date).month(),
         )
     })
+
+    it('should call Prisma with correct params', async () => {
+        const prismaSpy = jest.spyOn(prisma.transaction, 'findMany')
+        const sut = new PostgresGetTransactionsByUserIdRepository()
+
+        await sut.execute(user.id)
+
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                user_id: user.id,
+            },
+        })
+    })
 })
