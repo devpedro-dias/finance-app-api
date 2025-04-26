211fb5d4094f8f7bd4a7c32714150732ff61c20f
Pedro Dias <pedrocorchogdev@gmail.com>
2025-04-26 05:41:46 -0300

    test: ensure PostgresUpdateUserRepository calls Prisma with correct params

--- src/repositories/postgres/user/get-user-by-email.test.js
+++ src/repositories/postgres/user/get-user-by-email.test.js
@@ -2,7 +2,7 @@ import { prisma } from '../../../../prisma/prisma'
 import { user as fakeUser } from '../../../tests/fixtures'
 import { PostgresGetUserByEmailRepository } from './get-user-by-email'
 
-describe('GetUserByEmailRepository', () => {
+describe('PostgresGetUserByEmailRepository', () => {
     it('should get user by email on db', async () => {
         const user = await prisma.user.create({ data: fakeUser })
 
