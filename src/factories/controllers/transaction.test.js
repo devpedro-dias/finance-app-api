6bb2e2ee6e09645964d23dbb40a51996fd390a15
Pedro Dias <pedrocorchogdev@gmail.com>
2025-04-27 00:53:56 -0300

    test: ensure makeGetUserBalanceController return valid GetUserBalanceController

--- src/factories/controllers/transaction.test.js
+++ src/factories/controllers/transaction.test.js
@@ -12,25 +12,25 @@ import {
 } from './transaction'
 
 describe('Transaction Controller Factories', () => {
-    it('should return a valid CreateTransactionController instace', () => {
+    it('should return a valid CreateTransactionController instance', () => {
         expect(makeCreateTransactionController()).toBeInstanceOf(
             CreateTransactionController,
         )
     })
 
-    it('should return a valid UpdateTransactionController instace', () => {
+    it('should return a valid UpdateTransactionController instance', () => {
         expect(makeUpdateTransactionController()).toBeInstanceOf(
             UpdateTransactionController,
         )
     })
 
-    it('should return a valid DeleteTransactionController instace', () => {
+    it('should return a valid DeleteTransactionController instance', () => {
         expect(makeDeleteTransactionController()).toBeInstanceOf(
             DeleteTransactionController,
         )
     })
 
-    it('should return a valid GetTransactionsByUserIdController instace', () => {
+    it('should return a valid GetTransactionsByUserIdController instance', () => {
         expect(makeGetTransactionsByUserIdController()).toBeInstanceOf(
             GetTransactionsByUserIdController,
         )
