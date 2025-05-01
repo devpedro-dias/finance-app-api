91535cb39765f2239bc71776963a53992ee17829
Pedro Dias <pedrocorchogdev@gmail.com>
2025-05-01 05:50:34 -0300

    feat: ensure percentage per transaction is returned on /api/balance route

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -9,6 +9,7 @@ export class PostgresGetUserBalanceRepository {
                 lte: new Date(to),
             },
         }
+
         const {
             _sum: { amount: totalExpense },
         } = await prisma.transaction.aggregate({
@@ -52,15 +53,31 @@ export class PostgresGetUserBalanceRepository {
         const _totalExpense = totalExpense || new Prisma.Decimal(0)
         const _totalInvestment = totalInvestment || new Prisma.Decimal(0)
 
-        const balance = new Prisma.Decimal(
-            _totalEarning - _totalExpense - _totalInvestment,
-        )
+        const total = _totalEarning.plus(_totalExpense).plus(_totalInvestment)
 
+        const balance = _totalEarning
+            .minus(_totalExpense)
+            .minus(_totalInvestment)
+
+        const earningsPercentage = total.isZero()
+            ? 0
+            : _totalEarning.div(total).times(100).floor()
+
+        const expensesPercentage = total.isZero()
+            ? 0
+            : _totalExpense.div(total).times(100).floor()
+
+        const investmentsPercentage = total.isZero()
+            ? 0
+            : _totalInvestment.div(total).times(100).floor()
         return {
             earnings: _totalEarning,
             expenses: _totalExpense,
             investments: _totalInvestment,
             balance,
+            earningsPercentage,
+            expensesPercentage,
+            investmentsPercentage,
         }
     }
 }
