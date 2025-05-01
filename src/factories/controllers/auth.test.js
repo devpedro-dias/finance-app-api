0b103f3dc43fc378cf194d07bc922b2da9b7d218
Pedro Dias <pedrocorchogdev@gmail.com>
2025-05-01 07:39:29 -0300

    test: ensure makeRefreshTokenController returns valid RefreshTokenController

--- src/factories/controllers/auth.test.js
+++ src/factories/controllers/auth.test.js
@@ -1,8 +1,14 @@
-import { LoginUserController } from '../../controllers'
-import { makeLoginUserController } from './auth'
+import { LoginUserController, RefreshTokenController } from '../../controllers'
+import { makeLoginUserController, makeRefreshTokenController } from './auth'
 
 describe('Auth Controller Factories', () => {
     it('should return a valid makeLoginUserController instance', () => {
         expect(makeLoginUserController()).toBeInstanceOf(LoginUserController)
+    })
+
+    it('should return a valid makeRefreshTokenController instance', () => {
+        expect(makeRefreshTokenController()).toBeInstanceOf(
+            RefreshTokenController,
+        )
     })
 })
