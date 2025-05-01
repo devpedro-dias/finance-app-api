1bae49637a74f3916fbd2c5aba5cd32298f43b06
Pedro Dias <pedrocorchogdev@gmail.com>
2025-05-01 04:49:04 -0300

    feat: ensure GetUserBalanceController receives from and to params

--- src/controllers/user/get-user-balance.js
+++ src/controllers/user/get-user-balance.js
@@ -1,11 +1,12 @@
+import { ZodError } from 'zod'
+import { UserNotFoundError } from '../../errors/user.js'
+import { getUserBalanceSchema } from '../../schemas/user.js'
 import {
-    checkIfIdIsValid,
-    invalidIdResponse,
+    badRequest,
     ok,
     serverError,
     userNotFoundResponse,
 } from '../helpers/index.js'
-import { UserNotFoundError } from '../../errors/user.js'
 
 export class GetUserBalanceController {
     constructor(getUserBalanceUseCase) {
@@ -15,19 +16,27 @@ export class GetUserBalanceController {
     async execute(httpsRequest) {
         try {
             const userId = httpsRequest.params.userId
+            const from = httpsRequest.query.from
+            const to = httpsRequest.query.to
 
-            const idIsValid = checkIfIdIsValid(userId)
+            await getUserBalanceSchema.parseAsync({ user_id: userId, from, to })
 
-            if (!idIsValid) {
-                return invalidIdResponse()
-            }
-
-            const balance = await this.getUserBalanceUseCase.execute(userId)
+            const balance = await this.getUserBalanceUseCase.execute(
+                userId,
+                from,
+                to,
+            )
 
             return ok(balance)
         } catch (error) {
             if (error instanceof UserNotFoundError) {
                 return userNotFoundResponse()
+            }
+
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.errors[0].message,
+                })
             }
 
             console.error(error)
